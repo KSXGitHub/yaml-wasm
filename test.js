98b555682920ba7b20e41a6cc8129d8c80a5add8
khai96_ <hvksmr1996@gmail.com>
2020-04-02 11:11:07 +0700

    Declare a constructor

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import init, { Message } from './wasm-pack/yaml_wasm.js'
 await init()
-const message = Message.new('Hello, World!')
+const message = new Message('Hello, World!!')
 console.log(message)
 console.log(message.message())
