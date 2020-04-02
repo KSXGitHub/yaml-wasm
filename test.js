a02177cd452e2523eb496dd22e8617de212bf98d
khai96_ <hvksmr1996@gmail.com>
2020-04-02 11:01:34 +0700

    Target web

--- test.js
+++ test.js
@@ -1,2 +1,5 @@
-import * as all from './wasm-pack/yaml_wasm.js'
-console.log(all)
+import init, { Message } from './wasm-pack/yaml_wasm.js'
+await init()
+const message = Message.new('Hello, World!')
+console.log(message)
+console.log(message.message())
