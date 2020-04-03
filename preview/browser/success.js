53efd970ce3333ee22d84d643a0ca7a4c2efab79
khai96_ <hvksmr1996@gmail.com>
2020-04-03 10:18:54 +0700

    Embed yaml directly to JavaScript code

--- preview/browser/success.js
+++ preview/browser/success.js
@@ -1,9 +1,28 @@
 import init, { parse, stringify } from '../../index.js'
 
-init().then(async () => {
-  const response = await fetch('./valid-yaml')
-  console.log('%c parse()', 'font-size: 2em; color: red')
-  console.log(parse(await response.text()))
+const yamlText = `
+---
+int: 123
+float: 456.789
+text: hello
+array:
+  - abc
+  - def
+  - 123
+object:
+  foo: bar
+other_primitives:
+  - null
+  - true
+  - false
+---
+null
+---
+Hello There
+`
+
+init().then(() => {
+  console.log(parse(yamlText))
 
   console.log('%c stringify()', 'font-size: 2em; color: red')
   for (const item of [
