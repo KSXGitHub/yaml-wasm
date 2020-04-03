ca2d54372fe17212787b83e452a12e8d7a73cd33
khai96_ <hvksmr1996@gmail.com>
2020-04-03 16:16:07 +0700

    Refactor

--- src/stringify.rs
+++ src/stringify.rs
@@ -19,54 +19,60 @@ pub fn stringify(value: JsValue) -> Result<String, JsValue> {
 }
 
 fn js2yaml(js: JsValue) -> Yaml {
-    if js.is_function() || js.is_symbol() || js.is_undefined() {
-        return Yaml::BadValue;
-    }
+    fn compute<'js>(js: &'js JsValue, tracker: &mut Vec<&'js JsValue>) -> Yaml {
+        tracker.push(js);
 
-    if js.is_null() {
-        return Yaml::Null;
-    }
-
-    if js.is_string() {
-        return Yaml::String(js.as_string().unwrap());
-    }
-
-    if Number::is_integer(&js) {
-        return Yaml::Integer(js.as_f64().unwrap() as i64);
-    }
-
-    if Number::is_finite(&js) {
-        return Yaml::Real(js.as_f64().unwrap().to_string());
-    }
-
-    if js.is_object() {
-        return if Array::is_array(&js) {
-            let vec = Array::from(&js)
-                .iter()
-                .map(|x| js2yaml(x))
-                .collect::<Vec<Yaml>>();
-            Yaml::Array(vec)
-        } else {
-            use linked_hash_map::LinkedHashMap;
-            let mut hash = LinkedHashMap::new();
-            let entries = Array::from(&Object::entries(&Object::from(js)));
-            for entry in entries.iter() {
-                let pair = Array::from(&entry);
-                let key = pair.get(0);
-                let value = pair.get(1);
-                hash.insert(js2yaml(key), js2yaml(value));
-            }
-            Yaml::Hash(hash)
+        if js.is_function() || js.is_symbol() || js.is_undefined() {
+            return Yaml::BadValue;
         }
+
+        if js.is_null() {
+            return Yaml::Null;
+        }
+
+        if js.is_string() {
+            return Yaml::String(js.as_string().unwrap());
+        }
+
+        if Number::is_integer(&js) {
+            return Yaml::Integer(js.as_f64().unwrap() as i64);
+        }
+
+        if Number::is_finite(&js) {
+            return Yaml::Real(js.as_f64().unwrap().to_string());
+        }
+
+        if js.is_object() {
+            return if Array::is_array(&js) {
+                let vec = Array::from(&js)
+                    .iter()
+                    .map(|x| js2yaml(x))
+                    .collect::<Vec<Yaml>>();
+                Yaml::Array(vec)
+            } else {
+                use linked_hash_map::LinkedHashMap;
+                let mut hash = LinkedHashMap::new();
+                let entries = Array::from(&Object::entries(&Object::from(js.clone())));
+                for entry in entries.iter() {
+                    let pair = Array::from(&entry);
+                    let key = pair.get(0);
+                    let value = pair.get(1);
+                    hash.insert(js2yaml(key), js2yaml(value));
+                }
+                Yaml::Hash(hash)
+            }
+        }
+
+        if js.is_falsy() {
+            return Yaml::Boolean(false);
+        }
+
+        if js.is_truthy() {
+            return Yaml::Boolean(true);
+        }
+
+        unreachable!();
     }
 
-    if js.is_falsy() {
-        return Yaml::Boolean(false);
-    }
-
-    if js.is_truthy() {
-        return Yaml::Boolean(true);
-    }
-
-    unreachable!();
+    compute(&js, &mut Vec::new())
 }
