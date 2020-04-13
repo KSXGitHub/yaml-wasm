bdb051d096e6c7648120ac43b0746a725662db21
khai96_ <hvksmr1996@gmail.com>
2020-04-13 17:13:07 +0700

    Always rebuild before test

--- run-tests.ts
+++ run-tests.ts
@@ -1,8 +1,10 @@
 #! /usr/bin/env -S deno -A
 import { dirname } from 'https://deno.land/x/dirname/mod.ts'
+import { sh } from 'https://deno.land/x/drake@v0.16.0/mod.ts'
 import run from 'https://raw.githubusercontent.com/KSXGitHub/deno_run_tests_on_localhost/0.0.1/run.ts'
 const workDir = dirname(import.meta)
 Deno.chdir(workDir)
+await sh('make lib')
 const hostname = '0.0.0.0'
 const port = 4321
 const { code } = await run({ hostname, port })
