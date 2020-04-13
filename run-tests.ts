2ac301cf5ac9f72f88ef1f46a08c74c7f6dc3e37
khai96_ <hvksmr1996@gmail.com>
2020-04-13 11:05:39 +0700

    Update test runner to 0.0.1

--- run-tests.ts
+++ run-tests.ts
@@ -1,8 +1,9 @@
 #! /usr/bin/env -S deno -A
 import { dirname } from 'https://deno.land/x/dirname/mod.ts'
-import run from 'https://raw.githubusercontent.com/KSXGitHub/deno_run_tests_on_localhost/0.0.0/run.ts'
+import run from 'https://raw.githubusercontent.com/KSXGitHub/deno_run_tests_on_localhost/0.0.1/run.ts'
 const workDir = dirname(import.meta)
+Deno.chdir(workDir)
 const hostname = '0.0.0.0'
 const port = 4321
-const { code } = await run({ workDir, hostname, port })
+const { code } = await run({ hostname, port })
 throw Deno.exit(code)
