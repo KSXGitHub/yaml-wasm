ae5292acc5799cbace043d963f260f0a4f9da2ad
khai96_ <hvksmr1996@gmail.com>
2020-04-16 11:22:47 +0700

    Enable sccache for Travis CI

---
+++ ci/install-sccache.zsh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+if [[ -z $SCCACHE_VERSION ]]; then
+  echo '$SCCACHE_VERSION is not defined' > /dev/stderr
+  exit 1
+fi
+
+mkdir /sccache || exit $?
+
+prefix=https://github.com/mozilla/sccache/releases/download
+url=$prefix/$SCCACHE_VERSION/sccache-$SCCACHE_VERSION-x86_64-unknown-linux-musl.tar.gz
+binary=/sccache/sccache-$SCCACHE_VERSION-x86_64-unknown-linux-musl/sccache
+
+echo Downloading and extracting $url
+curl -fL# $url | tar -xvzf - -O /sccache || exit $?
+
+echo Installing sccache
+chmod a+x $binary
+exec ln -s $binary /bin/sccache
